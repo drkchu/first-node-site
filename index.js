b1f2f3d2a282ecbedd924cab60effa8b0d52b976
drkchu <derekdbchu@gmail.com>
2024-08-14 09:00:52 -0700

    refactor: using express.js instead of a bunch of switch statements

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 
-const port = 8080; // Port 8080 is commonly used as an alternative HTTP port for web servers. While the default port for HTTP traffic is 80, using port 8080 allows a server to run multiple web applications or services on the same machine.
+const app = express();
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-    let filePath = '';
+app.use(express.static(path.join(__dirname)));
 
-    switch (req.url) {
-        case '/':
-            filePath = 'index.html';
-            break;
-        case '/about':
-            filePath = 'about.html';
-            break;
-        case '/contact':
-            filePath = 'contact.html';
-            break;
-        default:
-            filePath = '404.html';
-            break;
-    }
-
-    const fullPath = path.join(__dirname, filePath); // environment variable that tells you the absolute path of the directory containing the currently executing file.
-
-    fs.readFile(fullPath, (err, content) => {
-        if (err) {
-            res.writeHead(500, {'Content-Type': 'text/html'}); // 500 Internal Server Error
-            res.end('<h1>500 Server Error</h1>', 'utf-8');
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'}); // success
-            res.end(content, 'utf-8');
-        }
-    });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(port, () => { // Basically the server is listening for when a connection happens
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+// Handle 404 errors
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+// Start the server
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
